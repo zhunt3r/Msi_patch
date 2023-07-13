b586ea9b236bbaff780b1d718a3c2337bcd1664d
Douglas Galdino <dougg0k@gmail.com>
2023-07-12 23:50:27 -0300

    updated minsudo script

--- scripts/optional_helpers/run_minsudo.cmd
+++ scripts/optional_helpers/run_minsudo.cmd
@@ -1,1 +1,4 @@
-%~dp0\..\tools\MinSudo --NoLogo --System --TrustedInstaller --Privileged cmd /c %1
+pushd "%~dp0"
+pushd ..\tools
+
+.\MinSudo --NoLogo --System --TrustedInstaller --Privileged cmd /c %1
